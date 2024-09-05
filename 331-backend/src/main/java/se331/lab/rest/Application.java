53594cb04ea7dcc16f468c0cf0a97dbb3206c2d4
DarbyAlway <141212791+DarbyAlway@users.noreply.github.com>
2024-09-06 01:45:42 +0700

    Add files via upload

---
+++ 331-backend/src/main/java/se331/lab/rest/Application.java
@@ -0,0 +1,26 @@
+package se331.lab.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Application {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/events")
+						.allowedOrigins("https://localhost:5173");
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
